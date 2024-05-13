9996a0b0a86b1e1e82bd7704c4e0c030a18a373d
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-05-13 17:43:56 -0300

    Set up for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from './pages/Home';
+import { Outlet } from "react-router-dom";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
       <Navigation />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Outlet />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Home from './pages/Home.tsx';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/frontend-nitin/",
+    element: <App />,
+    children: [
+      {
+        path: "/frontend-nitin/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
